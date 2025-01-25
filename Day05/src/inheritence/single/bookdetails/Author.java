96f0289c34d31715be164b8a7c8e2d3cbc56ff24
Ankit Patel <yugap1806@gmail.com>
2025-01-25 12:29:58 +0530

    Single Inheritence : Book and Author Deatails

---
+++ Day05/src/inheritence/single/bookdetails/Author.java
@@ -0,0 +1,16 @@
+package inheritence.single.bookdetails;
+
+public class Author extends Book {
+    String name;
+    String bio;
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Author Bio: " + bio);
+    }
+}
---
+++ Day05/src/inheritence/single/bookdetails/BookDetails.java
@@ -0,0 +1,8 @@
+package inheritence.single.bookdetails;
+
+public class BookDetails {
+    public static void main(String[] args) {
+        Author author = new Author("One Piece", 1997, "Eiichiro Oda", "Japanese Manga Artist ");
+        author.displayInfo();
+    }
+}
