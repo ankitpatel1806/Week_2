17a28a1d16b28d1eb81f653fee09c5bf5a88b67e
Ankit Patel <yugap1806@gmail.com>
2025-01-25 12:31:12 +0530

    Single Inheritence : Smart home device Deatails

---
+++ Day05/src/inheritence/single/devicedetails/Thermostat.java
@@ -0,0 +1,16 @@
+package inheritence.single.devicedetails;
+
+public class Thermostat extends Device {
+    int temperatureSetting;
+
+    public Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
